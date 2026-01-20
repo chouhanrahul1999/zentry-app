defc2fb34e349e7c2cd2ab320e1a0c67c0499ebe
RahulSingh <chouhanrahul1999@gmail.com>
2026-01-20 13:21:15 +0530

    created the static section of hero section

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+interface ButtonProps {
+  id: string;
+  title: string;
+  containerClass: string;
+  leftIcon?: React.ReactNode;
+}
+
+const Button = ({ id, title, containerClass, leftIcon }: ButtonProps) => {
+  return (
+    <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
+      {leftIcon}
+      
+      <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
+        <div>
+          {title}
+        </div>
+      </span>
+    </button>
+  )
+};
+
+export default Button;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,6 @@
 import { useRef, useState } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -29,42 +31,73 @@ const Hero = () => {
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden bg-blue-75"
       >
-        <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-          <div
-            onClick={handleMinVdClick}
-            className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
-          >
-            <video
-              src={getVideoSrc(upcamingVideoIndex)}
-              ref={nextVedioRef}
-              loop
-              muted
-              id="current-video"
-              className="size-64 origin-center scale-150 object-cover object-center"
-              onLoadedData={handleVideoLoad}
+        <div>
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+            <div
+              onClick={handleMinVdClick}
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+            >
+              <video
+                src={getVideoSrc(upcamingVideoIndex)}
+                ref={nextVedioRef}
+                loop
+                muted
+                id="current-video"
+                className="size-64 origin-center scale-150 object-cover object-center"
+                onLoadedData={handleVideoLoad}
+              />
+            </div>
+          </div>
+          <video
+            src={getVideoSrc(currentIndex)}
+            ref={nextVedioRef}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolute z-20 size-64 object-center"
+            onLoadedData={handleVideoLoad}
+          />
+
+          <video
+            src={getVideoSrc(
+              currentIndex === totalvideos - 1 ? 1 : currentIndex,
+            )}
+            autoPlay
+            loop
+            muted
+            id="next-video"
+            className="absolute left-0 top-0 size-full object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+        </div>
+
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+          G<b>a</b>ming
+        </h1>
+
+        <div className="absolute leading-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              redefi<b>n</b>e
+            </h1>
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100 leading-normal ">
+              Enter the Metagame Layer <br /> Unleash the Play Economy
+            </p>
+
+            <Button
+              id="watch-trailer"
+              title="Watch Trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass="!bg-yellow-300 flex-center gap-1"
             />
           </div>
         </div>
-        <video
-          src={getVideoSrc(currentIndex)}
-          ref={nextVedioRef}
-          loop
-          muted
-          id="next-video"
-          className="absolute-center invisible absolute z-20 size-64 object-center"
-          onLoadedData={handleVideoLoad}
-        />
-
-        <video
-          src={getVideoSrc(currentIndex === totalvideos - 1 ? 1 : currentIndex)}
-          autoPlay
-          loop
-          muted
-          id="next-video"
-          className="absolute left-0 top-0 size-full object-cover object-center"
-          onLoadedData={handleVideoLoad}
-        />
       </div>
+
+      <h1 className="special-font hero-heading absolute bottom-5 right-5  text-black">
+        G<b>a</b>ming
+      </h1>
+      
     </div>
   );
 };
