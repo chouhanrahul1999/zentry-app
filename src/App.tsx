f02363b614542c6349062196352a9d3b2fb3b6b8
RahulSingh <chouhanrahul1999@gmail.com>
2026-01-20 23:06:42 +0530

    complated images rigenstion using gsap

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import About from "./components/About"
 import Hero from "./components/Hero"
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
    <main className="relative min-h-screen w-screen overflow-x-hidden">
     <Hero />
+    <About />
     <section className="z-0 min-h-screen bg-blue-500"/>
    </main>
   )
