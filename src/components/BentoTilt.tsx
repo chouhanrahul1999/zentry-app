93c0de4057060c139eb7f8bdfeabd7148efb502f
RahulSingh <chouhanrahul1999@gmail.com>
2026-01-22 00:10:32 +0530

    added hovewe animation to to more card and incresed the the animation sencivity  featers card

--- src/components/BentoTilt.tsx
+++ src/components/BentoTilt.tsx
@@ -19,8 +19,8 @@ const BentoTilt = ({ children, className = "" }: BentoTiltProps) => {
     const relativeX = (e.clientX - left) / width;
     const relativeY = (e.clientY - top) / height;
 
-    const tiltX = (relativeY - 0.5) * 5;
-    const tiltY = (relativeX - 0.5) * -5;
+    const tiltX = (relativeY - 0.5) * 8;
+    const tiltY = (relativeX - 0.5) * -8;
 
     const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.98, 0.98, 0.98)`;
 
